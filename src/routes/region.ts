2c27398b5c22c1ef9cd8ea7807549e2f31d61003
cruzmart <cruzmart@ualberta.ca>
2025-03-15 17:55:31 -0600

    forgot to add redis caching. will def add it just for consistenty and to confirm it works for diff objects. alos, on my checking, i do need to go over all the query params to ensure they work and make them fubctional. Will do additonal redis caching and finish the search param as well as check over the query params today. tommorow will be going over all endpoints, adding openapi swagger to them all. Last is auth and then main app

--- src/routes/region.ts
+++ src/routes/region.ts
@@ -7,9 +7,9 @@ import {
 
 const router = Router();
 
-router.get('/', getAllRegions);
-router.get('/:region_id', getRegionById);
-router.get('/:region_name/', getRegionByRegionName);
+router.get('/', getAllRegions); // GET /chop/api/regions: Description: Fetch all available regions. Query parameters: sort, limit, page, search.
+router.get('/:region_id', getRegionById); // GET /chop/api/regions/{id}: Description: Fetch region by region Id. No Query parameters
+router.get('/:region_name/', getRegionByRegionName); // GET /chop/api/regions/{region_name}: Description: Fetch region by region name. No Query parameters
 router.get('/:region_id/nations', getRegionNationsByRegionId);
 router.get('/:region_name/nations', getRegionNationsByRegionName);
 
