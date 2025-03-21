7950a44e71f732068486475a0261ddf0c7b17cd3
cruzmart <cruzmart@ualberta.ca>
2025-03-20 22:16:04 -0600

    trying to fix swagger static files unseen

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-// import app from './app';
+import app from './app';
 
-// const PORT = process.env.PORT || 3000;
+if (process.env.IS_DEV==="True"){
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-import app from './app'; // Import your app
-
+}
 export default app;
