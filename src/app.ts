ef6c2e86f7578b941050735fc28f442b49247198
cruzmart <cruzmart@ualberta.ca>
2025-03-22 19:44:19 -0600

    the css shows but the endpoints do not. trying some reordering of app.use to see if any cahnge

--- src/app.ts
+++ src/app.ts
@@ -22,42 +22,6 @@ app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use(logger); // Use the custom logging middleware
 
-// swagger
-import swaggerUI from 'swagger-ui-express';
-//import swaggerDocument from './swagger.json';
-const swaggerUICss = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
-
-app.use(openapiDocURL,
-    swaggerUI.serve,
-    swaggerUI.setup(swaggerSpec, {
-        customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-        customCssUrl: swaggerUICss
-    }))
-// end of swagger
-
-
-// Check if sequelize is already initialized
-if (sequelize) {
-  console.log("Attempting to sync...");
-
-  // Add the Sequelize sync logic here
-  /* 
-   * Use { force: true } only in development to reset the database,
-   * Use { alter: true } or migrations in production to avoid losing data and to allow for schema updates without dropping tables.
-   */
-  setupAssociations();
-  sequelize.sync({ force: true }) 
-    .then(() => {
-      console.log("Database synced!");
-      //setupAssociations();
-    })
-    .catch((error) => {
-      console.error("Error syncing database:", error);
-    });
-} else {
-  console.error('Sequelize instance is not initialized(app).');
-}
-
 // Add your routes here
 // Define your routes here (e.g., for Recipes, Ingredients, etc.)
 app.get(`${baseURL}`, (req, res) => {
@@ -85,5 +49,42 @@ app.use(`${baseURL}/nations`, nationRoutes); // NATIONS
 
 import authRoutes from './routes/App/auth'; 
 app.use(`${baseURL}/auth`, authRoutes); // AUTH
+
+
+// swagger
+import swaggerUI from 'swagger-ui-express';
+//import swaggerDocument from './swagger.json';
+const swaggerUICss = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
+
+app.use(openapiDocURL,
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerSpec, {
+        customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+        customCssUrl: swaggerUICss
+}))// end of swagger
+
+
+// Check if sequelize is already initialized
+if (sequelize) {
+  console.log("Attempting to sync...");
+
+  // Add the Sequelize sync logic here
+  /* 
+   * Use { force: true } only in development to reset the database,
+   * Use { alter: true } or migrations in production to avoid losing data and to allow for schema updates without dropping tables.
+   */
+  setupAssociations();
+  sequelize.sync({ force: true }) 
+    .then(() => {
+      console.log("Database synced!");
+      //setupAssociations();
+    })
+    .catch((error) => {
+      console.error("Error syncing database:", error);
+    });
+} else {
+  console.error('Sequelize instance is not initialized(app).');
+}
+
 
 export default app;
