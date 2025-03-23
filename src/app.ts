272fbd0bf8529afdc18069a83c67d28717ec8508
cruzmart <cruzmart@ualberta.ca>
2025-03-22 19:50:40 -0600

    testing removing addition routes in vercel.json

--- src/app.ts
+++ src/app.ts
@@ -22,35 +22,6 @@ app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use(logger); // Use the custom logging middleware
 
-// Add your routes here
-// Define your routes here (e.g., for Recipes, Ingredients, etc.)
-app.get(`${baseURL}`, (req, res) => {
-  res.send("Welcome to the ChopAPI!");
-});
-
-// ROUTES
-import recipeRoutes from './routes/recipe'; 
-app.use(`${baseURL}/recipes`, recipeRoutes); // RECIPES
-
-import ingredientRoutes from './routes/ingredient'; 
-app.use(`${baseURL}/ingredients`, ingredientRoutes); // INGREDIENTS
-
-import categoryRoutes from './routes/category'; 
-app.use(`${baseURL}/categories`, categoryRoutes); // CATEGORIES
-
-import subcategoryRoutes from './routes/subcategory'; 
-app.use(`${baseURL}/subcategories`, subcategoryRoutes); // SUBCATEGORIES
-
-import regionRoutes from './routes/region'; 
-app.use(`${baseURL}/regions`, regionRoutes); // REGIONS
-
-import nationRoutes from './routes/nation'; 
-app.use(`${baseURL}/nations`, nationRoutes); // NATIONS
-
-import authRoutes from './routes/App/auth'; 
-app.use(`${baseURL}/auth`, authRoutes); // AUTH
-
-
 // swagger
 import swaggerUI from 'swagger-ui-express';
 //import swaggerDocument from './swagger.json';
@@ -86,5 +57,32 @@ if (sequelize) {
   console.error('Sequelize instance is not initialized(app).');
 }
 
+// Add your routes here
+// Define your routes here (e.g., for Recipes, Ingredients, etc.)
+app.get(`${baseURL}`, (req, res) => {
+  res.send("Welcome to the ChopAPI!");
+});
+
+// ROUTES
+import recipeRoutes from './routes/recipe'; 
+app.use(`${baseURL}/recipes`, recipeRoutes); // RECIPES
+
+import ingredientRoutes from './routes/ingredient'; 
+app.use(`${baseURL}/ingredients`, ingredientRoutes); // INGREDIENTS
+
+import categoryRoutes from './routes/category'; 
+app.use(`${baseURL}/categories`, categoryRoutes); // CATEGORIES
+
+import subcategoryRoutes from './routes/subcategory'; 
+app.use(`${baseURL}/subcategories`, subcategoryRoutes); // SUBCATEGORIES
+
+import regionRoutes from './routes/region'; 
+app.use(`${baseURL}/regions`, regionRoutes); // REGIONS
+
+import nationRoutes from './routes/nation'; 
+app.use(`${baseURL}/nations`, nationRoutes); // NATIONS
+
+import authRoutes from './routes/App/auth'; 
+app.use(`${baseURL}/auth`, authRoutes); // AUTH
 
 export default app;
