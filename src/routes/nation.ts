131cba8ce755a21fd6cd2e2ec79819046f7664c2
cruzmart <cruzmart@ualberta.ca>
2025-03-24 20:32:14 -0600

    all the recipe endpoint swagger docs

--- src/routes/nation.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllNations } from "../controllers/nationController";
-
-const router = Router();
-
-router.get('/', getAllNations); // GET /chop/api/nations: Description: Fetch all available nations. Query parameters: sort, limit, page, search.
-
-export default router;
-
