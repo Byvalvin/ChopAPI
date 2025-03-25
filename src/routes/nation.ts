c2945acb54bb4a92701093450d89a8c35003d9c2
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2025-03-24 20:34:01 -0600

    Create nation.ts

---
+++ src/routes/nation.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getAllNations } from "../controllers/nationController";
+
+const router = Router();
+
+router.get('/', getAllNations); // GET /chop/api/nations: Description: Fetch all available nations. Query parameters: sort, limit, page, search.
+
+export default router;
+
